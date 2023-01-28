6fe6bb547b7ff66d485bd55ea20b92c7ac5d5d48
Naveen <naveenkckalkatte437@gmail.com>
2023-01-28 22:55:19 +0530

    all done finalllyuu

--- lib/controllers/itemData.dart
+++ lib/controllers/itemData.dart
@@ -2,6 +2,14 @@ import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 
+
+/*Here I used "Provider" library for State Management because in Provider, widgets listen
+to changes in the state and update as soon as they are notified.
+Therefore, instead of the entire widget tree rebuilding when there is a state change,
+only the affected widget is changed, thus reducing the amount of work and making the app
+run faster and more smoothly.*/
+
+
 class ItemData extends ChangeNotifier
 {
 
