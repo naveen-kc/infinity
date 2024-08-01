2ea3bb5736b03477de09880cdfad5576f755d070
naveen-kc <86185619+naveen-kc@users.noreply.github.com>
2024-08-01 12:49:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+https://teams.microsoft.com/l/meetup-join/19%3ameeting_NjY0NGEyNTMtYmZmNi00YjA4LTg2MjItMDEzNzdkOWNiZTI5%40thread.v2/0?context=%7b%22Tid%22%3a%225ddc331c-86bd-4762-87d4-a9311b9b6f7c%22%2c%22Oid%22%3a%2288e7348d-5108-446e-acf9-502668a6f9d5%22%7d
 import 'package:flutter/material.dart';
 import 'package:infinity/screens/cart.dart';
 import 'package:infinity/screens/details.dart';
